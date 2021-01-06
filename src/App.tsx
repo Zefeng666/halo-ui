63e6a5d413a0a1fa454a44e64f3978bdd7ac8696
mason.lu <mason.lu@cloudwise.com>
2021-01-06 23:26:58 +0800

    ADD 完成Button组件的基本样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React, {ReactElement} from 'react';
+import React from 'react';
 import './styles/index.scss'
-
+import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>Halo</Button>
+        <Button btnType={ButtonType.Primary}>Primary</Button>
+        <Button btnType={ButtonType.Default}>Default</Button>
+        <Button btnType={ButtonType.Danger}>Danger</Button>
+        <Button btnType={ButtonType.Link} href={"https://www.baidu.com"}>Link</Button>
+        <Button disabled>Disabled</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary-Large</Button>
+        <Button btnType={ButtonType.Default} size={ButtonSize.Large}>Default-Large</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Danger-Large</Button>
+        <Button btnType={ButtonType.Link} size={ButtonSize.Large}>Link-Large</Button>
         <h1>Halo</h1>
         <h2>Halo</h2>
         <h3>Halo</h3>
