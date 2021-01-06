8b6719eef804f77a265d86341dba01887f1b01dc
mason.lu <mason.lu@cloudwise.com>
2021-01-06 18:15:15 +0800

    ADD 添加样式文件，并整合在index.scss中引入

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ReactElement} from 'react';
+import './styles/index.scss'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Halo</h1>
+        <h2>Halo</h2>
+        <h3>Halo</h3>
+        <hr/>
+        <code>
+          const a = b;
+        </code>
+
       </header>
     </div>
   );
