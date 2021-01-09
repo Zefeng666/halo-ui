4660659ea7a7b8d0ff9e54859fb4b5a11ab096d5
mason.lu <mason.lu@cloudwise.com>
2021-01-09 11:52:01 +0800

    ADD 增加Alert关闭

--- src/components/Alert/alert.tsx
+++ src/components/Alert/alert.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classNames from 'classnames';
 
 export interface AlertProps {
@@ -7,17 +7,22 @@ export interface AlertProps {
     description?: string,
 }
 
+
 const Alert: React.FC<AlertProps> = (props) => {
     const { message, description, type } = props;
+
+    let [close,setClose] = useState(true)
+
     const classes = classNames('alert', {
         [`alert-${type}`]: type,
     })
-    return <div className={classes}>
+    return (close? <div className={classes}>
         <div className='alert-content'>
             <div className='alert-msg'>{message}</div>
             <div className='alert-des'>{description}</div>
         </div>
-    </div>
+        <div className="alert-close" onClick={()=>setClose(false)}>关闭</div>
+    </div> : null)
 }
 
 Alert.defaultProps = {
